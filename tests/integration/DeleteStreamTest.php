e7a8c641cca49f6315d8be04683c305f688f97b8
prolic <saschaprolic@googlemail.com>
2017-07-07 20:29:24 +0800

    add delete stream integration test

---
+++ tests/integration/DeleteStreamTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the prooph/event-store-http-api.
+ * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\Http\Api\Integration;
+
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * @group integration
+ */
+class DeleteStreamTest extends AbstractHttpApiServerTestCase
+{
+    /**
+     * @test
+     */
+    public function it_deletes_stream(): void
+    {
+        $this->createTestStream();
+
+        $request = new Request('POST', 'http://localhost:8080/delete/teststream');
+
+        $response = $this->client->sendRequest($request);
+
+        $this->assertSame(204, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_delete_unknown_stream(): void
+    {
+        $request = new Request('POST', 'http://localhost:8080/delete/unknown');
+
+        $response = $this->client->sendRequest($request);
+
+        $this->assertSame(404, $response->getStatusCode());
+    }
+}
