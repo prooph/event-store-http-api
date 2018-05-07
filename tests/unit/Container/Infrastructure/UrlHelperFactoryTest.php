e1417e0e773ab88a91b7bf0657511a8f02027bc2
codeliner <kontakt@codeliner.ws>
2018-05-07 19:28:25 +0200

    Test ResponseFactory and UrlHelperFactory

---
+++ tests/unit/Container/Infrastructure/UrlHelperFactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the prooph/event-store-http-api.
+ * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\Http\Api\Unit\Container\Infrastructure;
+
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStore\Http\Api\Container\Infrastructure\UrlHelperFactory;
+use Prophecy\Argument;
+use Psr\Container\ContainerInterface;
+use Zend\Expressive\Helper\UrlHelper as ZendUrlHelper;
+
+class UrlHelperFactoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_wraps_zend_url_helper()
+    {
+        $zendUrlHelper = $this->prophesize(ZendUrlHelper::class);
+
+        $zendUrlHelper->generate(Argument::exact('user'), Argument::exact(['id' => 1]))->willReturn('/user/1')->shouldBeCalled();
+
+        $container = $this->prophesize(ContainerInterface::class);
+
+        $container->get(Argument::exact(ZendUrlHelper::class))->willReturn($zendUrlHelper->reveal())->shouldBeCalled();
+
+        $factory = new UrlHelperFactory();
+
+        $url = $factory->__invoke($container->reveal())->generate('user', ['id' => 1]);
+
+        $this->assertSame('/user/1', $url);
+    }
+}
---
+++ tests/unit/Middleware/ResponseFactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the prooph/event-store-http-api.
+ * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\Http\Api\Unit\Middleware;
+
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStore\Http\Api\Middleware\ResponseFactory;
+
+class ResponseFactoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_provides_response_object()
+    {
+        $response = (new ResponseFactory())->createResponse(201);
+
+        $this->assertSame(201, $response->getStatusCode());
+    }
+}
